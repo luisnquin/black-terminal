f1a62c608a439dcb69ea4ff9cfcd3be2ec82551a
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-22 10:58:15 -0500

    feat(hm/tmux): allow to mod tmux theme plugin

--- hm/components/tmux/default.nix
+++ hm/components/tmux/default.nix
@@ -5,39 +5,48 @@
   ...
 }: let
   cfg = config.shared.tmux;
-in
-  with lib; {
-    options.shared.tmux = {
-      enable = mkEnableOption "Shared tmux";
-    };
+  inherit (lib) mkEnableOption mkOption types mkIf;
+in {
+  options.shared.tmux = {
+    enable = mkEnableOption "Shared tmux configuration";
 
-    config = mkIf cfg.enable {
-      programs.tmux = {
-        enable = true;
-        package = pkgs.tmux;
-        extraConfig = builtins.readFile ./tmux.conf;
-
-        plugins = with pkgs; [
-          tmuxPlugins.pain-control
-          tmuxPlugins.sensible
-          tmuxPlugins.logging
-          tmuxPlugins.copycat
-          {
-            plugin = tmuxPlugins.tokyo-night-tmux;
-            extraConfig = ''
-              set -g @tokyo-night-tmux_theme night
-              set -g @tokyo-night-tmux_transparent 0
-            '';
-          }
-        ];
+    theme = {
+      plugin = mkOption {
+        type = types.package;
+        default = pkgs.tmuxPlugins.tokyo-night-tmux;
+        description = "Package for the tmux theme.";
       };
 
-      home.packages = [
-        pkgs.gitmux
+      extraConfig = mkOption {
+        type = types.lines;
+        default = ''
+          set -g @tokyo-night-tmux_theme night
+          set -g @tokyo-night-tmux_transparent 0
+          set -g @theme_variation 'storm'
+          set -g @theme_left_separator ''
+          set -g @theme_right_separator ''
+        '';
+        description = "Additional theme-specific tmux configuration.";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.tmux = {
+      enable = true;
+      extraConfig = builtins.readFile ./tmux.conf;
+
+      plugins = with pkgs.tmuxPlugins; [
+        pain-control
+        sensible
+        logging
+        copycat
+        cfg.theme
       ];
-
-      xdg.configFile = {
-        ".gitmux.conf".text = builtins.readFile ./gitmux.conf;
-      };
     };
-  }
+
+    home.packages = [pkgs.gitmux];
+
+    xdg.configFile."gitmux.conf".source = ./gitmux.conf;
+  };
+}

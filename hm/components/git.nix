b5cb6f93127884e2e1a54f08cddd2978a61c4fc2
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-09 00:16:46 -0500

    feat(hm/git): add hook to rm cursor trailer
    Cursor CLI is underrated but it still lacks a lot of config options like one
    to disable this (declaratively)

--- hm/components/git.nix
+++ hm/components/git.nix
@@ -9,6 +9,15 @@
   sharedGitConfig = import ../../shared/git/config.nix;
   gitOptions = import ../../shared/git/options.nix {inherit lib;};
   sharedGitShellAliases = import ../../shared/git/shell-aliases.nix;
+
+  removeCursorCoauthorHook = pkgs.writeShellScript "git-commit-msg-remove-cursor-coauthor" ''
+    set -euo pipefail
+    msg_file="$1"
+
+    ${lib.getExe pkgs.gnused} -i '/^Co-authored-by: Cursor <cursoragent@cursor\.com>$/d' "$msg_file"
+
+    ${lib.getExe pkgs.perl} -0pi -e 's/\n{3,}\z/\n\n/' "$msg_file"
+  '';
 in
   with lib; {
     options.shared.git = gitOptions;
@@ -38,6 +47,10 @@ in
           // {
             inherit (cfg) user;
           };
+
+        hooks = {
+          commit-msg = removeCursorCoauthorHook;
+        };
       };
 
       home.shellAliases = sharedGitShellAliases;
